80f11b64c12282251c51c5de118d734c25f50590
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-09-15 11:31:09 -0400

    commit tree ff802a0e0d003b91f38482218303bf6c7924cdf8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+let createEmployeeRecord = function (line) {
+    return {
+        firstName: line [0],
+        familyName: line [1],
+        title: line [2],
+        payPerHour: line [3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+};
+
+let createEmployeeRecords = function (employeeArray) {
+    return employeeArray.map(line => createEmployeeRecord(line))
+}
+
+let createTimeInEvent = function () {
+    
+}
